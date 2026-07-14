0746f63f05a13d9a6fe8c91c181eea22ac16d0c9
muturi <aaron06muturi@gmail.com>
2026-07-14 12:58:36 +0300

    Day-0

--- puzzles.py
+++ puzzles.py
@@ -3,18 +3,6 @@
 ## Imports 
 import random 
 
-## Adding the base (formatting)
-SUBSCRIPTS = {
-    "0": "₀", "1": "₁", "2": "₂", "3": "₃", "4": "₄",
-    "5": "₅", "6": "₆", "7": "₇", "8": "₈", "9": "₉"
-}
-
-def to_subscript(string):
-    return "".join(SUBSCRIPTS.get(char, char) for char in string)
-
-def format_with_base(number, base):
-    return f"{number}{to_subscript(str(base))}"
-
 ## The number generators:
 
 ### The interger number generators 
@@ -31,16 +19,29 @@ def p_float_number_generator(): # positive float number generator
 def n_float_number_generator(): # negative float number generator
     return round(random.uniform(-256, -1), 3) # includes -256 and -1
 
+## Adding the base (formatting)
+SUBSCRIPTS = {
+    "0": "₀", "1": "₁", "2": "₂", "3": "₃", "4": "₄",
+    "5": "₅", "6": "₆", "7": "₇", "8": "₈", "9": "₉"
+}
+
+def to_subscript(string):
+    return "".join(SUBSCRIPTS.get(char, char) for char in string)
+
+def format_with_base(number, base):
+    return f"{number}{to_subscript(str(base))}"
+
 # Question functions:
 
 ## The denary to binary conversion questions 
 def den_to_bin_question():
     # Back end calculations
-    denary = p_integer_number_generator()
-    binary = bin(denary)[2:] 
-    answer = format_with_base(binary, 2)
+    denary = p_integer_number_generator() # generate a random positive integer
+    binary = bin(denary)[2:] # convert to binary (python has a built in function) and remove the '0b' prefix
+
 
     # Front end (formatting the question and response)
+    answer = format_with_base(binary, 2)
     question = f"Convert {format_with_base(denary, 10)} to binary."
     response = f"The answer is {answer}"
     return question, response
@@ -52,9 +53,10 @@ def bin_to_den_question():
     # Back end calculations
     denary = p_integer_number_generator() # generate a random positive integer
     binary = bin(denary)[2:] # convert to binary (python has a built in function) and remove the '0b' prefix
-    answer = format_with_base(denary, 10)
+
 
     # Front end (formatting the question and response)
+    answer = format_with_base(denary, 10)
     question = f"Convert {format_with_base(binary, 2)} to denary."
     response = f"The answer is {answer}"
     return question, response
@@ -63,8 +65,12 @@ print(bin_to_den_question())
 
 ## The denary to octal conversion questions
 def den_to_oct_question():
+    # Back end calculations 
     denary = p_integer_number_generator() # generate a random positive integer
     octal = oct(denary)[2:] # convert to octal (python has a built in function) and remove the '0o' prefix
+    
+
+    # Front end (formatting the question and response)
     answer = format_with_base(octal, 8)
     question = f"Convert {format_with_base(denary, 10)} to octal."
     response = f"The answer is {answer}"
