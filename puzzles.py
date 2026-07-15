8eed4c392a13a346690d442a4d3375c019e08269
muturi <aaron06muturi@gmail.com>
2026-07-15 12:21:30 +0300

    Day0

--- puzzles.py
+++ puzzles.py
@@ -32,48 +32,281 @@ def format_with_base(number, base):
     return f"{number}{to_subscript(str(base))}"
 
 # Question functions:
+# Denary to binary, binary to denary, denary to octal, octal to denary, denary to hexadecimal, hexadecimal to denary
 
 ## The denary to binary conversion questions 
-def den_to_bin_question():
+def den_to_bin():
     # Back end calculations
     denary = p_integer_number_generator() # generate a random positive integer
     binary = bin(denary)[2:] # convert to binary (python has a built in function) and remove the '0b' prefix
 
 
     # Front end (formatting the question and response)
-    answer = format_with_base(binary, 2)
-    question = f"Convert {format_with_base(denary, 10)} to binary."
-    response = f"The answer is {answer}"
-    return question, response
+    ## Question
+    def den_to_bin_question():
+        question = f"Convert {format_with_base(denary, 10)} to binary."
+        return question
+    
+    ## Response
+    def den_to_bin_response():
+        answer = format_with_base(binary, 2)
+        response = f"The answer is {answer}"
+        return response
+    
+    return den_to_bin_question(), den_to_bin_response()
 
-print(den_to_bin_question())
+# print(den_to_bin())
 
 ## The binary to denary conversion questions
-def bin_to_den_question():
+def bin_to_den():
     # Back end calculations
     denary = p_integer_number_generator() # generate a random positive integer
     binary = bin(denary)[2:] # convert to binary (python has a built in function) and remove the '0b' prefix
 
 
     # Front end (formatting the question and response)
-    answer = format_with_base(denary, 10)
-    question = f"Convert {format_with_base(binary, 2)} to denary."
-    response = f"The answer is {answer}"
-    return question, response
+    ## Question
+    def bin_to_den_question():
+        question = f"Convert {format_with_base(binary, 2)} to denary."
+        return question
+    
+    ## Response
+    def bin_to_den_response():
+        answer = format_with_base(denary, 10)
+        response = f"The answer is {answer}"
+        return response
+    
+    return bin_to_den_question(), bin_to_den_response()
 
-print(bin_to_den_question())
+# print(bin_to_den())
 
 ## The denary to octal conversion questions
-def den_to_oct_question():
-    # Back end calculations 
+def den_to_oct():
+    # Back end calculations
     denary = p_integer_number_generator() # generate a random positive integer
     octal = oct(denary)[2:] # convert to octal (python has a built in function) and remove the '0o' prefix
-    
+
 
     # Front end (formatting the question and response)
-    answer = format_with_base(octal, 8)
-    question = f"Convert {format_with_base(denary, 10)} to octal."
-    response = f"The answer is {answer}"
-    return question, response
+    ## Question
+    def den_to_oct_question():
+        question = f"Convert {format_with_base(denary, 10)} to octal."
+        return question
+    
+    ## Response
+    def den_to_oct_response():
+        answer = format_with_base(octal, 8)
+        response = f"The answer is {answer}"
+        return response
+    
+    return den_to_oct_question(), den_to_oct_response()
 
-print(den_to_oct_question())
+# print(den_to_oct())
+
+## The octal to denary conversion questions
+def oct_to_den():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    octal = oct(denary)[2:] # convert to octal (python has a built in function) and remove the '0o' prefix
+
+
+    # Front end (formatting the question and response)
+    ## Question
+    def oct_to_den_question():
+        question = f"Convert {format_with_base(octal, 8)} to denary."
+        return question
+    
+    ## Response
+    def oct_to_den_response():
+        answer = format_with_base(denary, 10)
+        response = f"The answer is {answer}"
+        return response
+    
+    return oct_to_den_question(), oct_to_den_response()
+
+# print(oct_to_den())
+
+## The denary to hexadecimal conversion questions
+def den_to_hex():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    hexadecimal = hex(denary)[2:].upper() # convert to hexadecimal [uppercase] (python has a built in function) and remove the '0x' prefix
+
+
+    # Front end (formatting the question and response)
+    ## Question
+    def den_to_hex_question():
+        question = f"Convert {format_with_base(denary, 10)} to hexadecimal."
+        return question
+    
+    ## Response
+    def den_to_hex_response():
+        answer = format_with_base(hexadecimal, 16)
+        response = f"The answer is {answer}"
+        return response
+    
+    return den_to_hex_question(), den_to_hex_response()
+
+# print(den_to_hex())
+
+## The hexadecimal to denary conversion questions
+def hex_to_den():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    hexadecimal = hex(denary)[2:].upper() # convert to hexadecimal (python has a built in function) and remove the '0x' prefix
+
+
+    # Front end (formatting the question and response)
+    ## Question
+    def hex_to_den_question():
+        question = f"Convert {format_with_base(hexadecimal, 16)} to denary."
+        return question
+    
+    ## Response
+    def hex_to_den_response():
+        answer = format_with_base(denary, 10)
+        response = f"The answer is {answer}"
+        return response
+    
+    return hex_to_den_question(), hex_to_den_response()
+
+# print(hex_to_den()) 
+
+# Binary to octal, octal to binary, binary to hexadecimal, hexadecimal to binary
+
+def bin_to_oct():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    binary = bin(denary)[2:] # convert to binary (python has a built in function) and remove the '0b' prefix
+    octal = oct(denary)[2:] # convert to octal (python has a built in function) and remove the '0o' prefix
+
+    # Front end (formatting the question and response)
+    ## Question
+    def bin_to_oct_question():
+        question = f"Convert {format_with_base(binary, 2)} to octal."
+        return question
+    
+    ## Response
+    def bin_to_oct_response():
+        answer = format_with_base(octal, 8)
+        response = f"The answer is {answer}"
+        return response
+    
+    return bin_to_oct_question(), bin_to_oct_response()
+
+# print(bin_to_oct())
+
+def oct_to_bin():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    binary = bin(denary)[2:] # convert to binary (python has a built in function) and remove the '0b' prefix
+    octal = oct(denary)[2:] # convert to octal (python has a built in function) and remove the '0o' prefix
+
+    # Front end (formatting the question and response)
+    ## Question
+    def oct_to_bin_question():
+        question = f"Convert {format_with_base(octal, 8)} to binary."
+        return question
+    
+    ## Response
+    def oct_to_bin_response():
+        answer = format_with_base(binary, 2)
+        response = f"The answer is {answer}"
+        return response
+    
+    return oct_to_bin_question(), oct_to_bin_response()
+
+# print(oct_to_bin())
+
+def bin_to_hex():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    binary = bin(denary)[2:] # convert to binary (python has a built in function) and remove the '0b' prefix
+    hexadecimal = hex(denary)[2:].upper() # convert to hexadecimal [uppercase] (python has a built in function) and remove the '0x' prefix
+
+    # Front end (formatting the question and response)
+    ## Question
+    def bin_to_hex_question():
+        question = f"Convert {format_with_base(binary, 2)} to hexadecimal."
+        return question
+    
+    ## Response
+    def bin_to_hex_response():
+        answer = format_with_base(hexadecimal, 16)
+        response = f"The answer is {answer}"
+        return response
+    
+    return bin_to_hex_question(), bin_to_hex_response()
+
+# print(bin_to_hex())
+
+def hex_to_bin():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    binary = bin(denary)[2:] # convert to binary (python has a built in function) and remove the '0b' prefix
+    hexadecimal = hex(denary)[2:].upper() # convert to hexadecimal [uppercase] (python has a built in function) and remove the '0x' prefix
+
+    # Front end (formatting the question and response)
+    ## Question
+    def hex_to_bin_question():
+        question = f"Convert {format_with_base(hexadecimal, 16)} to binary."
+        return question
+    
+    ## Response
+    def hex_to_bin_response():
+        answer = format_with_base(binary, 2)
+        response = f"The answer is {answer}"
+        return response
+    
+    return hex_to_bin_question(), hex_to_bin_response()
+
+# print(hex_to_bin())
+
+# Octal to hexadecimal, hexadecimal to octal
+
+def oct_to_hex():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    octal = oct(denary)[2:] # convert to octal (python has a built in function) and remove the '0o' prefix
+    hexadecimal = hex(denary)[2:].upper() # convert to hexadecimal [uppercase] (python has a built in function) and remove the '0x' prefix
+
+    # Front end (formatting the question and response)
+    ## Question
+    def oct_to_hex_question():
+        question = f"Convert {format_with_base(octal, 8)} to hexadecimal."
+        return question
+    
+    ## Response
+    def oct_to_hex_response():
+        answer = format_with_base(hexadecimal, 16)
+        response = f"The answer is {answer}"
+        return response
+    
+    return oct_to_hex_question(), oct_to_hex_response()
+
+# print(oct_to_hex())
+
+def hex_to_oct():
+    # Back end calculations
+    denary = p_integer_number_generator() # generate a random positive integer
+    octal = oct(denary)[2:] # convert to octal (python has a built in function) and remove the '0o' prefix
+    hexadecimal = hex(denary)[2:].upper() # convert to hexadecimal [uppercase] (python has a built in function) and remove the '0x' prefix
+
+    # Front end (formatting the question and response)
+    ## Question
+    def hex_to_oct_question():
+        question = f"Convert {format_with_base(hexadecimal, 16)} to octal."
+        return question
+    
+    ## Response
+    def hex_to_oct_response():
+        answer = format_with_base(octal, 8)
+        response = f"The answer is {answer}"
+        return response
+    
+    return hex_to_oct_question(), hex_to_oct_response()
+
+# print(hex_to_oct())
+
+# Two's complement questions
+
